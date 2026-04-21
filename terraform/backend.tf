00d19f6f63cb1f4006c8ba5a9572b38938fa240a
Tanish Arora <tanisharora2312@gmail.com>
2026-04-21 19:43:19 +0000

    feat: added s3 for remote state storage and dynamodb for state locking

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+    backend "s3" {
+        bucket = "devops-tf-state-tanish"
+        key = "devops-pipeline/terraform.tfstate"
+        region = "ap-south-1"
+        dynamodb_table = "terraform-locks"
+        encrypt = true
+    }
+}
